9503604a70fcbb88b7eea85ded1f3a57d9b72c0b
Pete Jarvis <pete.jarvis@tidalscale.com>
2015-08-19 23:23:25 -0700

    baseline commit

---
+++ 09_DevelopingDataProducts/00CourseWork/WorldBank/UI.R
@@ -0,0 +1,46 @@
+require(shiny)
+
+###############################################################################
+##
+##  shinyUI function
+##
+###############################################################################
+shinyUI(
+  pageWithSidebar(
+    headerPanel("World Bank Economic Indicators"),
+
+    sidebarPanel(
+      helpText("Data from the World Bank."),
+
+
+      conditionalPanel(
+        condition = "input.theTabs == 'firstTab' ",
+
+        h3('Time series of an indicator'),
+        selectInput("indicator","Select an indicator:", choices = nameToCode, selected = "Fertility rate, total (births per woman)"),
+        checkboxInput("logScale","Log scale",value = FALSE)
+      ),
+
+      conditionalPanel(
+        condition = "input.theTabs == 'secondTab' ",
+        h3('Bubble Chart'),
+        sliderInput("year","Select a year:",min = 1961, max = 2010, step = 1, value = 2000, format = "####",animate=TRUE, sep = ""),
+        selectInput("xAxis","Select the first variable:", choices = nameToCode, selected = "Fertility rate, total (births per woman)"),
+        selectInput("yAxis","Select the second variable:", choices = nameToCode, selected = "Life expectancy at birth, total (years)")
+
+      )
+
+    ),
+
+    mainPanel(
+      tabsetPanel(
+        tabPanel( "Time series", plotOutput("timeSeries"),
+                  verbatimTextOutput("descriptionTab1"), value = "firstTab"),
+        tabPanel( "Bubble Chart", plotOutput("bubbleChart"),
+                  verbatimTextOutput("descriptionTab2"), value = "secondTab"),
+        id = "theTabs"
+      )
+    )
+  )
+
+)
---
+++ 09_DevelopingDataProducts/00CourseWork/WorldBank/server.R
@@ -0,0 +1,107 @@
+require(shiny)
+require(ggplot2)
+
+options(scipen = 999)
+
+# load the data
+worldBank <- read.csv("./data/WDIDataDashboard.csv", sep = "\t", header = TRUE)
+
+
+###############################################################################
+##
+##  bubbleChart function
+##
+###############################################################################
+
+bubbleChart <- function(X,x,y,radius,colour, alpha = 0.6){
+  gp <- ggplot(X,aes_string(x = x, y = y ))
+  gp <- gp + geom_point(aes_string(size = radius, colour = colour), alpha = alpha)
+  gp <- gp + geom_point(aes_string(size = radius), colour = 'black', alpha = 1, shape = 1 )
+  gp <- gp + guides(colour = guide_legend(override.aes = list(alpha = 1, size = 5)))
+  gp
+}
+
+###############################################################################
+##
+##  printDescription function
+##
+###############################################################################
+
+printDescription <- function(name,description){
+  s <- name
+  s <- paste0(s, "\n", paste(rep("=",nchar(name)),collapse=''))
+  s <- paste0(s, "\n", description)
+  cat(s)
+}
+
+###############################################################################
+##
+##  ShinyServer...
+##
+###############################################################################
+
+shinyServer(function(input,output){
+
+  # time series of an indicator
+  output$timeSeries <-  renderPlot({
+    p <- ggplot(
+      worldBank,
+      aes_string( x = 'Year', y = input$indicator, colour = "Country"))
+    p <- p + geom_line()
+    p <- p + geom_point()
+    p <- p + ylab(codeToName[input$indicator])
+    p <- p + scale_color_brewer(type="qual",palette='Set1')
+    p <- p + guides(colour = guide_legend(override.aes = list(alpha = 1, size = 5)))
+    if(input$logScale) {
+      p <- p + scale_y_log10()
+    }
+    print(p)
+  })
+
+  # bubble chart
+  output$bubbleChart <- renderPlot({
+    p <- bubbleChart(
+      subset(worldBank, Year == input$year),
+      x = input$xAxis,
+      y = input$yAxis,
+      radius = "SP.POP.TOTL",
+      colour = "Country")
+
+    p <- p + scale_color_brewer(type="qual",palette='Set1')
+    p <- p + guides(size = guide_legend(title = codeToName['SP.POP.TOTL']))
+    p <- p + xlab(codeToName[input$xAxis])
+    p <- p + ylab(codeToName[input$yAxis])
+    p <- p + scale_size_area(max_size=14)
+    p <- p + ggtitle(paste("World Bank Economic Data - Year: ",input$year))
+    print(p)
+  })
+
+  # bubble chart
+  output$bubbleChart <- renderPlot({
+    p <- bubbleChart(
+      subset(worldBank, Year == input$year),
+      x = input$xAxis,
+      y = input$yAxis,
+      radius = "SP.POP.TOTL",
+      colour = "Country")
+
+    p <- p + scale_color_brewer(type="qual",palette='Set1')
+    p <- p + guides(size = guide_legend(title = codeToName['SP.POP.TOTL']))
+    p <- p + xlab(codeToName[input$xAxis])
+    p <- p + ylab(codeToName[input$yAxis])
+    p <- p + scale_size_area(max_size=14)
+    p <- p + ggtitle(paste("World Bank Economic Data - Year: ",input$year))
+    print(p)
+  })
+
+  output$descriptionTab1 <- renderPrint({
+    printDescription(codeToName[input$indicator], codeToDescription[input$indicator])
+  })
+
+  output$descriptionTab2 <- renderPrint({
+    printDescription(codeToName[input$xAxis], codeToDescription[input$xAxis])
+    cat("\n\n")
+    printDescription(codeToName[input$yAxis], codeToDescription[input$yAxis])
+  })
+
+})
